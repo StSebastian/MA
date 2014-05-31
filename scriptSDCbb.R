a333a3f59fe8631c37469835dac548ee885ebc6e
StSebastian <sebastian.st@mytum.de>
2014-05-31 17:45:41 +0200

    Version12

--- scriptSDCbb.R
+++ scriptSDCbb.R
@@ -1,5 +1,5 @@
-SDCset<-function(DatCol, AcCol, TaCol, ShareCol, SicAcCol, SicTaCol)
-{	get<-function()	data.frame(DatCol, AcCol, TaCol, ShareCol, 
+SDCset <- function(DatCol, AcCol, TaCol, ShareCol, SicAcCol, SicTaCol)
+{	get <- function()	data.frame(DatCol, AcCol, TaCol, ShareCol, 
 				SicAcCol, SicTaCol, stringsAsFactors = F)
 
 ##	function() 	namecolAc
@@ -21,7 +21,7 @@ SDCset<-function(DatCol, AcCol, TaCol, ShareCol, SicAcCol, SicTaCol)
 	SDCCol
 }
 
-def<-SDCset("SP1","SP4","SP8")
+def <- SDCset("SP1","SP4","SP8")
 
 
 ICCset <- function(DatCol, DscdCol, IccCol){
@@ -43,8 +43,8 @@ ICCset <- function(DatCol, DscdCol, IccCol){
 }
 
 
-COMPset<-function(DscdCol, MvCol, SicCol)
-{	get<-function()	data.frame(DscdCol, MvCol, SicCol, stringsAsFactors=F)
+COMPset <- function(DscdCol, MvCol, SicCol)
+{	get <- function()	data.frame(DscdCol, MvCol, SicCol, stringsAsFactors=F)
 	
 	setDscd <- function(DSCD) DscdCol <<- DSCD
 	setMv   <- function(MV)  MvCol <<- MV 	
@@ -59,24 +59,49 @@ COMPset<-function(DscdCol, MvCol, SicCol)
 }
 
 
-SDCget<-function(SdcCol = SDCCol, SDCTable, SDCRow, EventW){
+EventWset <- function(PraeStart, PraeLast, PostStart, PostLast, MinObs)
+	{	
+	list(PraeStart = PraeStart, PraeLast = PraeLast, 
+		PostStart = PostStart, PostLast = PostLast,
+		MinObs = MinObs)
+	}
+
+
+EventWget <- function(Datum, EventW)
+	{
+	Datum <- as.POSIXlt(Datum) 
+	DatPost <- DatPrae <- rep(Datum,12)
 	
+	DatPrae$mon <- (EventW$PraeStart:EventW$PraeLast)+DatPrae$mon
+	DatPost$mon <- (EventW$PostStart:EventW$PostLast)+DatPost$mon
+	DatPrae <- substr(as.Date(DatPrae),1,7)
+	DatPost <- substr(as.Date(DatPost),1,7)
+	MinObs <- EventW$MinObs
+	
+	list(DatPrae = DatPrae, DatPost = DatPost, MinObs = MinObs)
+	}
+
+
+SDCget <- function(SdcCol = SDCCol, SDCTable, SDCRow, EVENTWset)
+	{
 	DatCol 	<- SdcCol$get()$DatCol ## "$DatCol" kommt von SDC get() data.frame	
 	AcquirorCol <- SdcCol$get()$AcCol
 	TargetCol   <- SdcCol$get()$TaCol
 	ShareAcCol  <- SdcCol$get()$ShareCol
 	SicAcqCol	<- SdcCol$get()$SicAcCol
-	SicTarCol    <- SdcCol$get()$SicTaCol
+	SicTarCol   <- SdcCol$get()$SicTaCol
+	
+	Datum     <- SDCTable[SDCRow,DatCol]
+	
+	MuaDat      <- EventWget(Datum, EVENTWset)
 
-	MuaDat       <- SDCTable[SDCRow,DatCol]
 	## MuaDat	 <- strptime(MuaDat,"%m.%d.%Y")
-		
-	MuaDatPrae <- as.POSIXlt(MuaDat)
-	MuaDatPrae$mon <- MuaDatPrae$mon - EventW
-	MuaDatPrae <- as.Date(MuaDatPrae)
-	MuaDatPost <- as.POSIXlt(MuaDat)
-	MuaDatPost$mon <- MuaDatPost$mon + EventW
- 	MuaDatPost <- as.Date(MuaDatPost)
+	##MuaDatPrae <- as.POSIXlt(MuaDat)
+	##MuaDatPrae$mon <- MuaDatPrae$mon - EventW
+	##MuaDatPrae <- as.Date(MuaDatPrae)
+	##MuaDatPost <- as.POSIXlt(MuaDat)
+	##MuaDatPost$mon <- MuaDatPost$mon + EventW
+ 	##MuaDatPost <- as.Date(MuaDatPost)
 
 	AcquirorDscd <- SDCTable[SDCRow,AcquirorCol]
 	TargetDscd   <- SDCTable[SDCRow,TargetCol]
@@ -84,40 +109,56 @@ SDCget<-function(SdcCol = SDCCol, SDCTable, SDCRow, EventW){
 	SicAc		 <- SDCTable[SDCRow,SicAcqCol]	
 	SicTa		 <- SDCTable[SDCRow,SicTarCol]	
 	
-	list( MuaDat= MuaDat, MuaDatPrae = MuaDatPrae, MuaDatPost = MuaDatPost, 
+	list( Datum = Datum, MuaDat = MuaDat, 
 		AcquirorDscd = AcquirorDscd, TargetDscd = TargetDscd, 
 		ShareAc = ShareAc, SicAc = SicAc, SicTa = SicTa)
 }
 
 
 ICCget <- function(IccCol = ICCCol, ICCTable, SDCget){
-	DatCol <- IccCol$get()$DatCol ## "$DatCol" kommt von ICC get() data.frame
+	DatCol  <- IccCol$get()$DatCol ## "$DatCol" kommt von ICC get() data.frame
 	DscdCol <- IccCol$get()$DscdCol
-	IccCol   <- IccCol$get()$IccCol
-	DatPrae <- SDCget$MuaDatPrae
-	DatPost <- SDCget$MuaDatPost
-	ICCTab <- ICCTable[,c(DatCol,DscdCol,IccCol)]
+	IccCol  <- IccCol$get()$IccCol
+	DatPrae <- SDCget$MuaDat$DatPrae
+	DatPost <- SDCget$MuaDat$DatPost
+	MinObs  <- SDCget$MuaDat$MinObs
+	ICCTab  <- ICCTable[,c(DatCol,DscdCol,IccCol)]
+	Abbruch <- list(TaIcc = NA, AcIccPrae = NA, AcIccPost = NA)
 	if(is.na(SDCget$TargetDscd)|is.na(SDCget$AcquirorDscd))
-	{return (list(TaIcc = NA, AcIccPrae = NA, AcIccPost = NA))}
+	{return (Abbruch)}
 		
 		temp  <- ICCTab[, DscdCol] == SDCget$TargetDscd
-		temp  <- ICCTab[temp, c(DatCol,IccCol)]
-		tempDat <- substr(temp[,DatCol],1,7)== substr(DatPrae,1,7)
-		TaIcc <- temp[tempDat, IccCol]
+		tempTA  <- ICCTab[temp, c(DatCol,IccCol)]
+		tempDat <- is.element(substr(tempTA[,DatCol],1,7),DatPrae)
+		##if (sum(tempDat) < MinObs){return (Abbruch)}
+		TaIcc <- tempTA[tempDat, IccCol]
+		TaIcc <- TaIcc[!is.na(TaIcc)]
+		if(length(TaIcc) < MinObs){return (Abbruch)}
+		TaIcc <- mean(TaIcc, na.rm = TRUE)
 		
 		temp  <- ICCTab[, DscdCol] == SDCget$AcquirorDscd
 		tempAC  <- ICCTab[temp, c(DatCol,IccCol)]
-		tempDat <- substr(tempAC[,DatCol],1,7)== substr(DatPrae,1,7)
+		tempDat <- is.element(substr(tempAC[,DatCol],1,7),DatPrae)
+		##if (sum(tempDat) < MinObs){return (Abbruch)}
 		AcIccPrae <- tempAC[tempDat, IccCol]
-			
-		tempDat <- substr(tempAC[,DatCol],1,7)== substr(DatPost,1,7)
+		AcIccPrae <- AcIccPrae[!is.na(TaIcc)]
+		if(length(AcIccPrae) < MinObs){return (Abbruch)}
+		AcIccPrae <- mean(AcIccPrae, na.rm = TRUE)
+	
+		tempDat <- is.element(substr(tempAC[,DatCol],1,7),DatPost)
+		##if (sum(tempDat) < MinObs){return (Abbruch)}
 		AcIccPost <- tempAC[tempDat, IccCol]
+		AcIccPost <- AcIccPost[!is.na(AcIccPost)]
+		if(length(AcIccPost) < MinObs){return (Abbruch)}
+		AcIccPost <- mean(AcIccPost, na.rm = TRUE)
 		
-		if(any(sapply(list(AcIccPost,AcIccPrae,TaIcc),length)==0)
-			|any(is.na(c(AcIccPost,AcIccPrae,TaIcc))))
+		##if(any(sapply(list(AcIccPost,AcIccPrae,TaIcc),length)==0)
+		##	|any(is.na(c(AcIccPost,AcIccPrae,TaIcc))))
+		##{return (Abbruch)}
+		
 		##if(length(AcIccPost)==0|length(AcIccPrae)==0|length(TaIcc)==0
 		##|any(is.na(c(AcIccPost,AcIccPrae,TaIcc))))
-		{return (list(TaIcc = NA, AcIccPrae = NA, AcIccPost = NA))}
+		##{return (Abbruch)}
 		
 		list(TaIcc = TaIcc, AcIccPrae = AcIccPrae, AcIccPost = AcIccPost)
 }
@@ -288,7 +329,7 @@ testF <- function(SDCTab,SDCCol,eventW){
 	test2 <- as.data.frame(matrix(rep(NA,6*laenge),nrow=laenge,ncol=6))
 	for (i in 1:laenge){ 
 	temp <- SDCget(SDCCol,SDCTab,i,eventW)
-	test2[i,] <- c(as.character(temp$MuaDat), temp$AcquirorDscd, temp$TargetDscd, 
+	test2[i,] <- c(as.character(temp$Datum), temp$AcquirorDscd, temp$TargetDscd, 
 			temp$ShareAc, temp$SicAc, temp$SicTa)	
 	}
 	names(test2)<-names(SDCCol$get())
@@ -296,7 +337,7 @@ testF <- function(SDCTab,SDCCol,eventW){
 	}
 
 testG <- function(SDCTab,SDCCol,ICCCol,ICCTab,eventW){
-	laenge <- 100
+	laenge <- 1800
 	test2 <- as.data.frame(matrix(rep(NA,9*laenge),nrow=laenge,ncol=9))
 	for (i in 1:laenge){ 
 	temp  <- SDCget(SDCCol,SDCTab,i,eventW)
@@ -306,7 +347,7 @@ testG <- function(SDCTab,SDCCol,ICCCol,ICCTab,eventW){
 	##temp2$AcIccPrae <- if(length(temp2$AcIccPrae) == 0)NA else temp2$AcIccPrae
 	##temp2$AcIccPost <- if(length(temp2$AcIccPost) == 0)NA else temp2$AcIccPost
 
-	test2[i,] <- c(as.character(temp$MuaDat), temp$AcquirorDscd, temp$TargetDscd, 
+	test2[i,] <- c(as.character(temp$Datum), temp$AcquirorDscd, temp$TargetDscd, 
 		temp$ShareAc, temp$SicAc , temp$SicTa ,
 		temp2$TaIcc, temp2$AcIccPrae, temp2$AcIccPost)	
 	}
